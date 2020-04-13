c9451abd7351ec38543357af42e856b39f6669f9
hammer09 <c.hamilton1507@gmail.com>
2020-04-13 14:42:28 +0100

    private final String DELETE_BOOKING_URL = "/delete/{id}"; added

--- src/main/java/com/finalyearproject/pitchbooking/controller/BookingController.java
+++ src/main/java/com/finalyearproject/pitchbooking/controller/BookingController.java
@@ -22,6 +22,11 @@ public class BookingController {
     private final String BOOKING_LIST_URL = "/bookings/list";
     private final String EDIT_BOOKING_URL = "/bookings/edit/{id}";
     private final String EDIT_BOOKING_TEMPLATE = "/bookings/edit";
+    private final String DELETE_BOOKING_URL = "/delete/{id}";
+    private final String VIEW_HOME_PAGE_URL = "/";
+    private final String SHOW_NEW_BOOKING_FORM_URL = "/new";
+    private final String SAVE_BOOKING_URL = "/save";
+    private final String UPDATE_BOOKING_URL = "/update/{id}";
 
     @Autowired
     private BookingService bookingService;
@@ -38,7 +43,7 @@ public class BookingController {
         return BOOKING_LIST_URL;
     }
 
-    private final String VIEW_HOME_PAGE_URL = "/";
+
     @GetMapping(VIEW_HOME_PAGE_URL )
     public String viewHomePage(Model model) {
         List<Booking> listBookings = bookingService.listAll();
@@ -47,7 +52,7 @@ public class BookingController {
         return "index";
     }
 
-    private final String SHOW_NEW_BOOKING_FORM_URL = "/new";
+
     @GetMapping(SHOW_NEW_BOOKING_FORM_URL)
     public String showNewBookingForm(Model model) {
         Booking booking = new Booking();
@@ -56,7 +61,7 @@ public class BookingController {
         return "bookings/create";
     }
 
-    private final String SAVE_BOOKING_URL = "/save";
+
     @PostMapping (SAVE_BOOKING_URL)
     public String saveBooking(@Valid @ModelAttribute("booking")
 
@@ -83,7 +88,37 @@ public class BookingController {
         return EDIT_BOOKING_TEMPLATE;
     }
 
-    private final String DELETE_BOOKING_URL = "/delete/{id}";
+
+    @PostMapping (UPDATE_BOOKING_URL)
+    public String updateBooking(@PathVariable(name = "id") Long id, @Valid @ModelAttribute("booking")
+                                      Booking booking,
+                              BindingResult bindingResult,Model model){
+
+        if (bindingResult.hasErrors()){
+            return EDIT_BOOKING_TEMPLATE + "/" + id;
+        }
+
+        //Find booking by id
+        //booking = bookingService.findById(booking.getId());
+        bookingService.save(booking);
+
+        return "redirect:/";
+    }
+
+//    @PostMapping("/update/{id}")
+//    public String updateUser(@PathVariable("id") long id, @Valid User user,
+//                             BindingResult result, Model model) {
+//        if (result.hasErrors()) {
+//            user.setId(id);
+//            return "update-user";
+//        }
+//
+//        userRepository.save(user);
+//        model.addAttribute("users", userRepository.findAll());
+//        return "index";
+//    }
+
+
     @GetMapping(DELETE_BOOKING_URL)
     public  String deleteBooking(@PathVariable(name = "id")long id) {
         bookingService.delete(id);
