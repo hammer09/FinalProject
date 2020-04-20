5068fa9cb69cef7f53c1ff277bb6c6a6c06a7599
hammer09 <c.hamilton1507@gmail.com>
2020-04-20 13:17:35 +0100

    new pitch class

--- src/main/java/com/finalyearproject/pitchbooking/model/Pitch.java
+++ src/main/java/com/finalyearproject/pitchbooking/model/Pitch.java
@@ -5,55 +5,89 @@ import java.util.HashSet;
 import java.util.Set;
 
 
-   @Entity
-   public class Pitch {
+@Entity
+public class Pitch {
 
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-        private String name;
+    private String name;
 
-        private String type;
+    private String type;
 
-        @OneToOne(mappedBy = "pitch", cascade = CascadeType.ALL)
+    @OneToOne
+    @JoinColumn
+    private Booking booking;
 
-        //private Set<Booking> booking = new HashSet<>();
+    public Long getId() {
+        return id;
+    }
 
-       private Booking booking;
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-        public Pitch() { }
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 
+    public String getType() {
+        return type;
+    }
 
-       public Long getId() {
-           return id;
-        }
-
-       public void setId(Long id) {
-            this.id = id;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public String getType() {
-            return type;
-        }
-
-       public void setType(String type) {
-            this.type = type;
-        }
+    public void setType(String type) {
+        this.type = type;
     }
 
 
+//private Pitch pitch;
 
+    //private Set<Booking> booking = new HashSet<>();
 
+    public Pitch() {
+    }
 
+    public Booking getBooking() {
+        return booking;
+    }
+
+    private boolean sameAsFormer(Booking newBooking) {
+        if (this.booking == null) {
+            return newBooking == null;
+        }
+        return this.booking.equals(newBooking);
+    }
+
+    public void setBooking(Booking booking) {
+
+        // prevent endless loop
+
+        if (sameAsFormer(booking)) {
+            return;
+        }
+
+        // set new booking
+
+        Booking oldBooking = this.booking;
+        this.booking = booking;
+
+        // remove from the old booking
+
+        if (oldBooking != null) {
+            oldBooking.setPitch(null);
+        }
+
+        // set record into new booking
+
+        if (booking != null) {
+            booking.setPitch(this);;
+        }
+    }
+}
 
 
